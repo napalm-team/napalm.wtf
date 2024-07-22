0a2f3a763e65c6e44330ce5b70f515b269ba1110
C. Pham <61342696+NopeNotDark@users.noreply.github.com>
2024-07-21 22:27:26 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="content">
-      <h1> Napalm <span className="beta">BETA</span> </h1>
+      <h1> Napalm <span className="beta">BETA</span> Loader </h1>
       <div className="download">
         <a href="/download" className="button">Download</a>
       </div>
