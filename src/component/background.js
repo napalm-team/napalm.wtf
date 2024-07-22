ce79d08e90671cec53ae3e134c889d25c682959c
C. Pham <61342696+NopeNotDark@users.noreply.github.com>
2024-07-21 22:59:26 -0400

    Update background.js

--- src/component/background.js
+++ src/component/background.js
@@ -1,13 +1,22 @@
 "use client";
 
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './background.css';
 
 export default function Background({ children }) {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const video = videoRef.current;
+        if (video) {
+            video.currentTime = 2;
+        }
+    }, []);
+
     return (
         <div className="background-wrapper">
             <div className="background-container">
-                <video className="background-video" autoPlay muted loop>
+                <video ref={videoRef} className="background-video" autoPlay muted loop>
                     <source src="video.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
