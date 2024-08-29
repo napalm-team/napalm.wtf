235c213c38ce0dae012d5138a73fd8b7a19c404b
C. Pham <61342696+NopeNotDark@users.noreply.github.com>
2024-08-28 21:40:04 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
+import React from 'react';
+
 export default function Home() {
+  const currentLocation = window.location.href;
+  const downloadLink = `${currentLocation}/download`;
+
   return (
     <div className="content">
-      <h1> Napalm <span className="beta">BETA</span> Loader </h1>
+      <h1>Napalm <span className="beta">BETA</span> Loader</h1>
       <div className="download">
-        <a href="/download" className="button">Download</a>
+        <a href={downloadLink} className="button">Download</a>
       </div>
     </div>
   );
