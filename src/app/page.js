01a922d69feb8a4d2226f32e90c5bbcf6ea305c5
C. Pham <61342696+NopeNotDark@users.noreply.github.com>
2024-08-30 10:09:26 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-
 export default function Home() {
-  const router = useRouter();
-  
-  const downloadLink = `${router.asPath}/download`;
-
   return (
     <div className="content">
-      <h1>Napalm <span className="beta">BETA</span> Loader</h1>
+      <h1> Napalm <span className="beta">BETA</span> Loader </h1>
       <div className="download">
-        <a href={downloadLink} className="button">Download</a>
+        <a href="/download" className="button">Download</a>
       </div>
     </div>
   );
-}
